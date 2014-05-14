c197720716341ae0bb349a030c6c14eb1b7243e1
simleo <simleo@crs4.it>
2014-05-14 12:52:57 +0200

    added copyright notice to source files

--- hadoop_galaxy/pathset.py
+++ hadoop_galaxy/pathset.py
@@ -1,22 +1,11 @@
-
-
-# Copyright (C) 2011-2014 CRS4.
-#
-# This file is part of Seal.
-#
-# Seal is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option)
-# any later version.
-#
-# Seal is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with Seal.  If not, see <http://www.gnu.org/licenses/>.
-
+# BEGIN_COPYRIGHT
+# 
+# Copyright (C) 2014 CRS4.
+# 
+# This file is part of hadoop-galaxy, released under the terms of the BSD
+# 3-Clause License <http://opensource.org/licenses/BSD-3-Clause>.
+# 
+# END_COPYRIGHT
 
 import argparse
 import os
--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
+# BEGIN_COPYRIGHT
+# 
 # Copyright (C) 2014 CRS4.
-#
+# 
+# This file is part of hadoop-galaxy, released under the terms of the BSD
+# 3-Clause License <http://opensource.org/licenses/BSD-3-Clause>.
+# 
+# END_COPYRIGHT
+
+"""
+Hadoop-Galaxy provides tools for integrating Hadoop and Galaxy.
+
+Hadoop: <http://hadoop.apache.org>
+Galaxy: <http://galaxyproject.org>
+"""
 
 import glob
 import os
@@ -12,9 +25,11 @@ from distutils.core import setup
 # chdir to project's root directory (where this file is located)
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
-NAME = 'hadoop_galaxy'
-URL = "http://www.crs4.it"
-# DOWNLOAD_URL = ""
+NAME = 'hadoop-galaxy'
+DESCRIPTION = __doc__.split("\n", 1)[0]
+LONG_DESCRIPTION = __doc__
+URL = "https://github.com/crs4/hadoop-galaxy"
+DOWNLOAD_URL = URL
 LICENSE = 'BSD'
 CLASSIFIERS = [
   "Programming Language :: Python",
@@ -39,10 +54,10 @@ MAINTAINER_EMAIL = ", ".join("<%s>" % t[1] for t in MAINTAINER_INFO)
 
 
 setup(name=NAME,
-   #description=DESCRIPTION,
-   #long_description=LONG_DESCRIPTION,
+   description=DESCRIPTION,
+   long_description=LONG_DESCRIPTION,
    url=URL,
-   #download_url=DOWNLOAD_URL,
+   download_url=DOWNLOAD_URL,
    license=LICENSE,
    classifiers=CLASSIFIERS,
    author=AUTHOR,
